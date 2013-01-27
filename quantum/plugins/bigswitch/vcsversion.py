97e05963106e505e28b648e8846896d8ea13c849
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2013-01-27 11:46:02 -0800

    Adding plugin version info.

---
+++ quantum/plugins/bigswitch/vcsversion.py
@@ -0,0 +1,9 @@
+version_info = {'branch_nick': u'grizzly/master',
+                'revision_id': u'1',
+                'revno': 0}
+
+
+QUANTUMRESTPROXY_VERSION = ['2013', '1', None]
+
+
+FINAL = False   # This becomes true at Release Candidate time

72753654da7db2b7f848819826e8735add87aa27
Sumit Naiksatam <sumitnaiksatam@gmail.com>
2013-04-06 11:05:56 -0700

    Adding resources for service chain, and db model implementation

---
+++ quantum/db/servicechains_db.py
@@ -0,0 +1,179 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Big Switch Networks, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Sumit Naiksatam, sumitnaiksatam@gmail.com, Big Switch Networks, Inc.
+
+import sqlalchemy as sa
+from sqlalchemy import orm
+from sqlalchemy.orm import exc
+
+from quantum.db import model_base
+from quantum.db import models_v2
+from quantum.extensions import servicechain
+from quantum.openstack.common import log as logging
+from quantum.openstack.common import uuidutils
+
+
+LOG = logging.getLogger(__name__)
+
+
+class ServiceChainTemplate(model_base.BASEV2, models_v2.HasId,
+                           models_v2.HasTenant):
+    """Represents a Service Chain Template resource"""
+    __tablename__ = 'service_chain_template'
+    name = sa.Column(sa.String(255))
+    description = sa.Column(sa.String(1024))
+    services_types = sa.Column(sa.VARCHAR)
+
+
+class ServiceChain(model_base.BASEV2, models_v2.HasId, models_v2.HasTenant):
+    """Represents a service resource"""
+    __tablename__ = 'service_chain'
+    name = sa.Column(sa.String(255))
+    description = sa.Column(sa.String(1024))
+    service_chain_template_id = sa.Column(sa.String(36), sa.ForeignKey(
+                                          'service_chain_template.id'))
+    services_chain = sa.Column(sa.VARCHAR)
+
+
+class ServiceChain_db_mixin(servicechain.ServiceChainPluginBase):
+    """Mixin class to add Service Chain methods to db_plugin_base_v2"""
+
+
+    def _get_service_chain_template(self, context, id):
+        try:
+            sct = self._get_by_id(context, ServiceChainTemplate, id)
+        except exc.NoResultFound:
+            raise servicechain.ServiceChainTemplateNotFound(template_id=id)
+        except exc.MultipleResultsFound:
+            LOG.error(_('Multiple service chain templates match for %s'), id)
+            raise servicechain.ServiceChainTemplateNotFound(template_id=id)
+        return sct
+
+    def _get_service_chain(self, context, id):
+        try:
+            sc = self._get_by_id(context, ServiceChain, id)
+        except exc.NoResultFound:
+            raise servicechain.ServiceChainNotFound(service_chain_id=id)
+        except exc.MultipleResultsFound:
+            LOG.error(_('Multiple service chains match for %s'), id)
+            raise servicechain.ServiceChainNotFound(service_chain_id=id)
+        return sc
+
+    def _make_service_chain_template_dict(self, sct, fields=None):
+        res = {'id': sct['id'],
+               'name': sct['name'],
+               'description': sct['description'],
+               'tenant_id': sct['tenant_id'],
+               'services_types_list': sct['services_types']}
+        return self._fields(res, fields)
+
+    def _make_service_chain_dict(self, sc, fields=None):
+        res = {'id': sc['id'],
+               'name': sc['name'],
+               'description': sc['description'],
+               'tenant_id': sc['tenant_id'],
+               'service_chain_template_id': sc['service_chain_template_id'],
+               'services_chain': sc['services_chain']}
+        return self._fields(res, fields)
+
+    def create_service_chain(self, context, service_chain):
+        sc = service_chain['service_chain']
+        tenant_id = self._get_tenant_id_for_create(context, sc)
+        with context.session.begin(subtransactions=True):
+            sc_db = ServiceChain(id=uuidutils.generate_uuid(),
+                                 tenant_id=tenant_id,
+                                 name=sc['name'],
+                                 description=sc['description'],
+                                 service_chain_template_id=
+                                 sc['service_chain_template_id'],
+                                 services_chain=
+                                 sc['services_chain_list'])
+            context.session.add(sc_db)
+        return self._make_service_chain_dict(sc_db)
+
+    def update_service_chain(self, context, id, service_chain):
+        sc = service_chain['service_chain']
+        with context.session.begin(subtransactions=True):
+            sc_db = self._get_service_chain(context, id)
+            # Ensure we actually have something to update
+            if sc.keys():
+                sc_db.update(sc)
+        return self._make_service_chain_dict(sc_db)
+
+    def delete_service_chain(self, context, id):
+        with context.session.begin(subtransactions=True):
+            sc = self._get_service_chain(context, id)
+            # TODO (Sumit) Ensure that the service_chain  is not
+            # being used
+            context.session.delete(sc)
+
+    def get_service_chain(self, context, id, fields=None):
+        sc = self._get_service_chain(context, id)
+        return self._make_service_chain_dict(sc, fields)
+
+    def get_service_chains(self, context, filters=None, fields=None):
+        return self._get_collection(context, ServiceChain,
+                                    self._make_service_chain_dict,
+                                    filters=filters, fields=fields)
+
+    def get_service_chains_count(self, context, filters=None):
+        return self._get_collection_count(context, ServiceChain,
+                                          filters=filters)
+
+    def create_service_chain_template(self, context, service_chain_template):
+        sct = service_chain_template['service_chain_template']
+        tenant_id = self._get_tenant_id_for_create(context, sct)
+        with context.session.begin(subtransactions=True):
+            sct_db = ServiceChainTemplate(id=uuidutils.generate_uuid(),
+                                          tenant_id=tenant_id,
+                                          name=sct['name'],
+                                          description=sct['description'],
+                                          services_types=
+                                          sct['services_type_list'])
+            context.session.add(sct_db)
+        return self._make_service_chain_template_dict(sct_db)
+
+    def update_service_chain_template(self, context, id,
+                                      service_chain_template):
+        sct = service_chain_template['service_chain_template']
+        with context.session.begin(subtransactions=True):
+            sct_db = self._get_service_chain_template(context, id)
+            # Ensure we actually have something to update
+            if sct.keys():
+                sct_db.update(sct)
+        return self._make_service_chain_template_dict(sct_db)
+
+    def delete_service_chain_template(self, context, id):
+        with context.session.begin(subtransactions=True):
+            sct = self._get_service_chain_template(context, id)
+            # TODO (Sumit) Ensure that the service_chain_template  is not
+            # being used
+            context.session.delete(sct)
+
+    def get_service_chain_template(self, context, id, fields=None):
+        sct = self._get_service_chain_template(context, id)
+        return self._make_service_chain_template_dict(sct, fields)
+
+    def get_service_chain_templates(self, context, filters=None, fields=None):
+        return self._get_collection(context, ServiceChainTemplate,
+                                    self._make_service_chain_template_dict,
+                                    filters=filters, fields=fields)
+
+    def get_service_chain_templates_count(self, context, filters=None):
+        return self._get_collection_count(context, ServiceChainTemplate,
+                                          filters=filters)
---
+++ quantum/extensions/servicechain.py
@@ -0,0 +1,223 @@
+# vim: tabstop=4 shiftwidth=4 softtabstop=4
+#
+# Copyright 2013 Big Switch Networks, Inc.
+# All Rights Reserved.
+#
+#    Licensed under the Apache License, Version 2.0 (the "License"); you may
+#    not use this file except in compliance with the License. You may obtain
+#    a copy of the License at
+#
+#         http://www.apache.org/licenses/LICENSE-2.0
+#
+#    Unless required by applicable law or agreed to in writing, software
+#    distributed under the License is distributed on an "AS IS" BASIS, WITHOUT
+#    WARRANTIES OR CONDITIONS OF ANY KIND, either express or implied. See the
+#    License for the specific language governing permissions and limitations
+#    under the License.
+#
+# @author: Sumit Naiksatam, sumitnaiksatam@gmail.com, Big Switch Networks, Inc.
+
+import abc
+
+from oslo.config import cfg
+
+from quantum.api import extensions
+from quantum.api.v2 import attributes as attr
+from quantum.api.v2 import base
+from quantum.common import exceptions as qexception
+from quantum import manager
+from quantum.openstack.common import uuidutils
+from quantum.plugins.common import constants
+from quantum.plugins.services.service_base import ServicePluginBase
+
+
+# Service Chain Exceptions
+class ServicesListEmpty(qexception.NotFound):
+    message = _("Services list cannot be empty")
+
+
+class ServicesTypeListEmpty(qexception.NotFound):
+    message = _("Services type list cannot be empty")
+
+
+class ServiceChainNotFound(qexception.NotFound):
+    message = _("Service chain %(service_chain_id)s could not be found")
+
+
+class ServiceChainInUse(qexception.InUse):
+    message = _("Service chain %(service_chain_id)s is still active")
+
+
+class ServiceChainTemplateNotFound(qexception.NotFound):
+    message = _("Service Chain Template %(template_id)s could not be found")
+
+
+service_types = ['L3', 'L2', 'BumpInTheWire', 'Tap']
+
+
+def _convert_to_uuid_list(value_list):
+    if value_list is None:
+        raise ServicesListEmpty()
+    for sc_id in value_list:
+        if not uuidutils.is_uuid_like(sc_id):
+            msg = _("'%s' is not an integer or uuid") % sc_id
+            raise qexception.InvalidInput(error_message=msg)
+    return value_list
+
+
+def _convert_to_service_type_list(value_list):
+    if value_list is None:
+        raise ServicesTypeListEmpty()
+    for st_id in value_list:
+        if st_id not in service_types:
+            msg = _("'%s' is not a valid service type") % st_id
+            raise qexception.InvalidInput(error_message=msg)
+    return value_list
+
+
+RESOURCE_ATTRIBUTE_MAP = {
+    'service_chain_templates': {
+        'id': {'allow_post': False, 'allow_put': False,
+               'validate': {'type:uuid': None},
+               'is_visible': True,
+               'primary_key': True},
+        'tenant_id': {'allow_post': True, 'allow_put': False,
+                      'required_by_policy': True,
+                      'is_visible': True},
+        'description': {'allow_post': True, 'allow_put': True,
+                        'validate': {'type:string': None},
+                        'is_visible': True, 'default': ''},
+        'name': {'allow_post': True, 'allow_put': True,
+                 'validate': {'type:string': None},
+                 'is_visible': True, 'default': ''},
+        'services_types_list': {'allow_post': True, 'allow_put': True,
+                                'is_visible': True,
+                                'convert_to': _convert_to_service_type_list},
+        'shared': {'allow_post': False, 'allow_put': False,
+                   'default': True, 'convert_to': attr.convert_to_boolean,
+                   'is_visible': True, 'required_by_policy': True,
+                   'enforce_policy': True},
+    },
+    'service_chains': {
+        'id': {'allow_post': False, 'allow_put': False,
+               'validate': {'type:uuid': None},
+               'is_visible': True,
+               'primary_key': True},
+        'tenant_id': {'allow_post': True, 'allow_put': False,
+                      'required_by_policy': True,
+                      'is_visible': True},
+        'description': {'allow_post': True, 'allow_put': True,
+                        'validate': {'type:string': None},
+                        'is_visible': True, 'default': ''},
+        'name': {'allow_post': True, 'allow_put': True,
+                 'validate': {'type:string': None},
+                 'is_visible': True, 'default': ''},
+        'template_id': {'allow_post': True, 'allow_put': True,
+                        'validate': {'type:uuid': None},
+                        'is_visible': True},
+        'services_list': {'allow_post': True, 'allow_put': True,
+                          'is_visible': True,
+                          'convert_to': _convert_to_uuid_list},
+    },
+}
+
+
+class ServiceChain(extensions.ExtensionDescriptor):
+    """ Security chain extension"""
+
+    @classmethod
+    def get_name(cls):
+        return "Service Chain"
+
+    @classmethod
+    def get_alias(cls):
+        return "service_chain"
+
+    @classmethod
+    def get_description(cls):
+        return "Extension for Services Chains"
+
+    @classmethod
+    def get_namespace(cls):
+        return "http://docs.openstack.org/ext/servicechains/api/v2.0"
+
+    @classmethod
+    def get_updated(cls):
+        return "2013-04-25T10:00:00-00:00"
+
+    @classmethod
+    def get_resources(cls):
+        my_plurals = [(key, key[:-1]) for key in RESOURCE_ATTRIBUTE_MAP.keys()]
+        attr.PLURALS.update(dict(my_plurals))
+        exts = []
+        plugin = manager.QuantumManager.get_plugin()
+        for resource_name in ['service_chain', 'service_chain_template']:
+            collection_name = resource_name.replace('_', '-') + "s"
+            params = RESOURCE_ATTRIBUTE_MAP.get(resource_name + "s", dict())
+            controller = base.create_resource(collection_name,
+                                              resource_name,
+                                              plugin, params, allow_bulk=True,
+                                              allow_pagination=True,
+                                              allow_sorting=True)
+
+            ex = extensions.ResourceExtension(collection_name,
+                                              controller,
+                                              attr_map=params)
+            exts.append(ex)
+
+        return exts
+
+    @classmethod
+    def get_plugin_interface(cls):
+        return ServiceChainPluginBase
+
+    def update_attributes_map(self, attributes):
+        super(ServiceChain, self).update_attributes_map(
+            attributes, extension_attrs_map=RESOURCE_ATTRIBUTE_MAP)
+
+    def get_extended_resources(self, version):
+        if version == "2.0":
+            return RESOURCE_ATTRIBUTE_MAP
+        else:
+            return {}
+
+
+class ServiceChainPluginBase(object):
+    __metaclass__ = abc.ABCMeta
+
+    @abc.abstractmethod
+    def get_service_chains(self, context, filters=None, fields=None):
+        pass
+
+    @abc.abstractmethod
+    def get_service_chain(self, context, id, fields=None):
+        pass
+
+    @abc.abstractmethod
+    def create_service_chain(self, context, service_chain):
+        pass
+
+    @abc.abstractmethod
+    def update_service_chain(self, context, id, service_chain):
+        pass
+
+    @abc.abstractmethod
+    def delete_service_chain(self, context, id):
+        pass
+
+    @abc.abstractmethod
+    def get_service_chain_templates(self, context, filters=None, fields=None):
+        pass
+
+    @abc.abstractmethod
+    def get_service_chain_template(self, context, id, fields=None):
+        pass
+
+    @abc.abstractmethod
+    def create_service_chain_template(self, context, service_chain_template):
+        pass
+
+    @abc.abstractmethod
+    def update_service_chain_template(self, context, id,
+                                      service_chain_template):
+        pass
